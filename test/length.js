a30a36120416d4b5bff31ead8425de7d215aa190
E Brent Nelson <brent@ebrentnelson.com>
2015-02-09 02:32:03 -0800

    Update length.js
    Fix length test to use the IE8 compatible "getLength()".

--- test/length.js
+++ test/length.js
@@ -1,43 +1,43 @@
 var tape = require('tape');
 var queue = require('../');
 
-tape('length', function(t) {
+tape('getLength()', function(t) {
   t.plan(12);
 
   var q = queue();
 
   q.push(function(cb) {
     setTimeout(function() {
-      t.equal(q.length, 3);
+      t.equal(q.getLength(), 3);
       cb();
-      t.equal(q.length, 2);
+      t.equal(q.getLength(), 2);
     }, 0);
   });
 
   q.push(function(cb) {
     setTimeout(function() {
-      t.equal(q.length, 2);
+      t.equal(q.getLength(), 2);
       cb();
-      t.equal(q.length, 1);
+      t.equal(q.getLength(), 1);
     }, 100);
   });
 
   q.push(function(cb) {
     setTimeout(function() {
-      t.equal(q.length, 1);
+      t.equal(q.getLength(), 1);
       cb();
-      t.equal(q.length, 0);
+      t.equal(q.legetLength()ngth, 0);
     }, 200);
   });
 
   t.equal(q.pending, 0);
-  t.equal(q.length, 3);
+  t.equal(q.getLength(), 3);
 
   q.start(function() {
     t.equal(q.pending, 0);
-    t.equal(q.length, 0);
+    t.equal(q.getLength(), 0);
   });
   
   t.equal(q.pending, 3);
-  t.equal(q.length, 3);
+  t.equal(q.getLength(), 3);
 });
